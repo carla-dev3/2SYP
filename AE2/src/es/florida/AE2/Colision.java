7d72426bfbd7fae1c770fd5a08cb5c3395689631
CarlaAparicio <carlaaparicio0905@gmail.com>
2021-10-22 00:19:41 +0200

    AE2 Puntos 2 y 3. Calculo de la probabilidad y gestion de ficheros individuales

--- AE2/src/es/florida/AE2/Colision.java
+++ AE2/src/es/florida/AE2/Colision.java
@@ -1,9 +1,57 @@
 package es.florida.AE2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Colision {
 
+	/**
+	 * Metodo: probabilidadColision
+	 * Descripcion: calcular la probabilidad de que el NEO colisione con la tierra y guardarla en ficheros independientes
+	 * 
+	 *  @param posicionNEO, velocidadNEO
+	 */
+    public double probabilidadColision(double posicionNEO, double velocidadNEO) {
+        double posicionTierra = 1;
+        double velocidadTierra = 100;
+        long inicio = System.nanoTime();
+
+        for (int i = 0; i < (50 * 365 * 24 * 60 * 60); i++) {
+            posicionNEO = posicionNEO + velocidadNEO * i;
+            posicionTierra = posicionTierra + velocidadTierra * i;
+        }
+
+        double resultado = 100 * Math.random() * Math.pow(((posicionNEO - posicionTierra) / (posicionNEO + posicionTierra)), 2);
+        BigDecimal bigDecimal = new BigDecimal(resultado).setScale(2, RoundingMode.UP);
+
+        return bigDecimal.doubleValue();
+    }
+
 	public static void main(String[] args) {
+		 Colision colision = new Colision();
+	        double posicionTierra = Float.parseFloat(args[0]);
+	        double velocidadTierra = Float.parseFloat(args[1]);
+	        FileWriter fichero = null;
+	        PrintWriter printer = null;
+	        try {
+	            fichero = new FileWriter(args[2]);
+	            printer = new PrintWriter(fichero);
+	            double resultado = colision.probabilidadColision(posicionTierra, velocidadTierra);
+	            printer.println(resultado);
+	            System.out.println("La probabilidad de colisión es de " + resultado);
+	        } catch (Exception e) {
+	            e.printStackTrace();
 
+	        } finally {
+	            try {
+	                if (null != fichero)
+	                    fichero.close();
+	            }catch (IOException e2) {
+	                e2.printStackTrace();
+	            }
+	      }
 	}
-
 }
