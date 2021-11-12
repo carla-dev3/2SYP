3fb8fab3aa397338dd83685e044ddd01b7599ba0
CarlaAparicio <carlaaparicio0905@gmail.com>
2021-11-12 11:59:29 +0100

    AE3 Método extraerRecursos con el tiempo de extracción

--- AE3/src/es/florida/AE3/Minero.java
+++ AE3/src/es/florida/AE3/Minero.java
@@ -18,10 +18,29 @@ public class Minero implements Runnable {
     public int getBolsa() {
         return bolsa;
     }
+    
+    private void extraerRecursos(String nombre) {
+        
+        if (mina.stock > 0) {
+            mina.stock--;
+            setBolsa(bolsa + 1);
+            System.out.println(nombre + " ha extraido 1 recurso de la mina");
+            System.out.println("Quedan " + mina.stock + " recursos en la mina");
+        } else {
+            System.out.println(nombre + " ha salido con las manos vacias");
+            System.out.println("No quedan suficientes recursos");
+        }
+        System.out.println("-------------------------------------------------");
 
 	@Override
 	public void run() {
-		
+		  try {
+	            Thread.sleep(tiempoExtraccion);
+	            String nombre = Thread.currentThread().getName();
+	            extraerRecursos(nombre);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
 	}
     
     
