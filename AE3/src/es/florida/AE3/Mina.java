b12968421cae88469bfed833d66f49526090d520
CarlaAparicio <carlaaparicio0905@gmail.com>
2021-11-12 11:52:31 +0100

    AE3 Clase Mina con la variable stock y su constructor

---
+++ AE3/src/es/florida/AE3/Mina.java
@@ -0,0 +1,14 @@
+package es.florida.AE3;
+
+public class Mina {
+	
+	   protected int stock;
+
+	    Mina() {
+	        this.stock = 50;
+	    }
+
+	    public int getStock() {
+	        return stock;
+    }
+}
