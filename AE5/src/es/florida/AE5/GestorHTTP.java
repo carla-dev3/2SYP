4b6851a2f4f8b9e702c00a89331bcca49b29b4bb
CarlaAparicio <carlaaparicio0905@gmail.com>
2022-01-20 20:07:42 +0100

    AE5 Modificaciones en el punto 5

--- AE5/src/es/florida/AE5/GestorHTTP.java
+++ AE5/src/es/florida/AE5/GestorHTTP.java
@@ -3,13 +3,13 @@ package es.florida.AE5;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class GestorHTTP implements HttpHandler {
 
     //La clase GestorHTTP tendrá dos atributos: temperaturaActual y temperaturaTermostato.
     //Inicialmente ambas temperaturas tendrán el mismo valor, por ejemplo 15º
+
     String temperaturaActual = "15";
     double temperaturaTermostato = 15;
 
@@ -20,8 +20,8 @@ public class GestorHTTP implements HttpHandler {
             temperaturaActual = handleGetRequest(httpExchange);
             handleGETResponse(httpExchange, temperaturaActual);
         } else if ("POST".equals(httpExchange.getRequestMethod())) {
-         //   requestParamValue = handlePostRequest(httpExchange);
-         //   handlePOSTResponse(httpExchange, requestParamValue);
+            temperaturaActual = handlePostRequest(httpExchange);
+            handlePOSTResponse(httpExchange, temperaturaActual);
         }
     }
 
@@ -30,13 +30,43 @@ public class GestorHTTP implements HttpHandler {
         return httpExchange.getRequestURI().toString().split("\\?")[1].split("=")[1];
     }
 
+    private String handlePostRequest(HttpExchange httpExchange) {
+        System.out.println("Recibida URI tipo POST: " + httpExchange.getRequestBody().toString());
+        InputStream is = httpExchange.getRequestBody();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
     private void handleGETResponse(HttpExchange httpExchange, String temperaturaActual) throws IOException {
         OutputStream outputStream = httpExchange.getResponseBody();
         String htmlResponse = "<html><body><h1>Temperatura Actual -> " + temperaturaActual + "</h1></body></html>";
         httpExchange.sendResponseHeaders(200, htmlResponse.length());
         outputStream.write(htmlResponse.getBytes());
+        System.out.println("Devuelve respuesta HTML: " + htmlResponse);
         outputStream.flush();
         outputStream.close();
-        System.out.println("Devuelve respuesta HTML: " + htmlResponse);
+
+    }
+
+    private void handlePOSTResponse(HttpExchange httpExchange, String temperaturaActual) throws IOException {
+        OutputStream outputStream = httpExchange.getResponseBody();
+        String htmlResponse = "Parametro/s POST: " + temperaturaActual + " -> Se procesará por parte del servidor";
+        System.out.println("Devuelve respueta HTML: " + htmlResponse);
+        httpExchange.sendResponseHeaders(200, htmlResponse.length());
+        outputStream.write(htmlResponse.getBytes());
+        outputStream.flush();
+        outputStream.close();
+
     }
 }
